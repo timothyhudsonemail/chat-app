261284e16e621b121720c418f49d7976c1ac4e4b
timothyhudsonemail <60154902+timothyhudsonemail@users.noreply.github.com>
2021-03-24 18:11:32 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -124,18 +124,22 @@ export default class App extends Component {
           alt="abstract art" />
         <br />
       <br />
-    <div style={{display:"flex",flexDirection:"row",justifyContent:"space-around"}}>
+    <div className='flex-container'>
+      <div className='flex-child'>
       <p style={{color:"rgb(255, 51, 153)",
                  fontFamily:"Avaraa",
                  paddingLeft:"90px",
                  paddingRight:"90px"}}>
       Welcome! This is a full stack chat application which facilitates communication between 2 remote users. Conversation persists between page reloads.</p>
+      </div>
+      <div className='flex-child'>
       <p style={{color:"rgb(255, 51, 153)",
                  fontFamily:"Avaraa",
                  paddingLeft:"90px",
                  paddingRight:"90px"}}>
       Technical info: This application is built using React. Front end hosted on Vercel (deployed via github), server built from scratch and hosted on Heroku. PSQL database hosted on aws s3.
       </p>
+    </div>
    </div>
     <br />
     
