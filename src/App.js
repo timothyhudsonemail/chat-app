3fa5a847dc9bc41d4d8990e17a806d7fd5ba2196
timothyhudsonemail <60154902+timothyhudsonemail@users.noreply.github.com>
2021-03-24 15:47:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -176,18 +176,18 @@ export default class App extends Component {
         <div className='flex-container'>
           <div className='flex-child'>
       <br />
-            <p style={{fontFamily:"Avaraa",color:"rgb(255, 51, 153)",paddingBottom:"30px"}}>Message from User 1:</p>
+            <p style={{fontFamily:"Avaraa",color:"rgb(255, 51, 153)",paddingBottom:"20px"}}>Message from User 1:</p>
           
       <h3 style={{fontFamily:"Avaraa"}}>{this.state.msg3 ? this.state.msg3 :'Message 1 will appear here'}</h3>
-      <br /><br />
+      <br /><br /><br /><br />
           </div>
 
           <div className='flex-child'> 
       <br />      
-      <p style={{fontFamily:"Avaraa",color:"rgb(255, 51, 153)",paddingBottom:"30px"}}>Message from User 2:</p>
+      <p style={{fontFamily:"Avaraa",color:"rgb(255, 51, 153)",paddingBottom:"20px"}}>Message from User 2:</p>
         
       <h3 style={{fontFamily:"Avaraa"}}>{this.state.msg4 ? this.state.msg4 : 'Message 2 will appear here'}</h3>
-      <br /><br />
+      <br /><br /><br /><br />
           </div>
         </div>
       </div>
